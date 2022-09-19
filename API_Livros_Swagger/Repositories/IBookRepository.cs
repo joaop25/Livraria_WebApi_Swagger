4485a9e0d91213d67c1569c9323d4796abd22bca
Joao Pedro Fernandes <82421334+joaop25@users.noreply.github.com>
2022-09-19 07:44:49 -0300

    Add files via upload

---
+++ API_Livros_Swagger/Repositories/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using API_Livros_Swagger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Livros_Swagger.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+
+        Task Delete(int id);
+
+
+    }
+}
